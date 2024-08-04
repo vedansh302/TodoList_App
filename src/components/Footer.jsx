df3f8a498a073ca23dc4b88f4b46bff7a7ea6355
Vedansh Shrivastava <vedanshwork302@gmail.com>
2024-08-04 13:07:00 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-900 text-white text-center bottom-0 fixed w-full'>
-      <p>&#169; 2024 New Delhi India, Inc. Allright Reserved</p>
+      <p>&#169; 2024 New Delhi India, Inc. | All Rights Reserved</p>
       <p>Made with &#9829; by Vedansh</p>
     </div>
   )
